feb2e357599fe5c542159c58cad8860fb2baa987
zaru <sakuraba@basicinc.jp>
2017-02-21 16:31:01 +0900

    Added vpc and gw

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region = "ap-northeast-2"
 }
+
+resource "aws_vpc" "myVPC" {
+    cidr_block = "10.1.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "false"
+    tags {
+      Name = "myVPC"
+    }
+}
+
+resource "aws_internet_gateway" "myGW" {
+    vpc_id = "${aws_vpc.myVPC.id}"
+}
